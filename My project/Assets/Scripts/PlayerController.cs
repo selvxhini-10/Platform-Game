4f1d1cc40a824937f55f1545babc0a757b58b3a8
selvxhini-10 <selvahinik10@gmail.com>
2025-01-27 19:17:06 -0500

    Add sample project

---
+++ My project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,124 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float walkSpeed = 5f;
+    public float runSpeed = 8f;
+    Vector2 moveInput;
+
+    public float CurrentMoveSpeed { get
+        {
+            if(IsMoving)
+            {
+                if(IsRunning)
+                {
+                    return runSpeed;
+                }
+                else
+                {
+                    return walkSpeed;
+                }
+            } else
+            {
+                return 0; //idle speed is 0
+            }
+        } }
+
+    [SerializeField]
+    private bool _isMoving = false;
+    public bool IsMoving { get
+        {
+            return _isMoving;
+        } private set
+        {
+            _isMoving = value;
+            animator.SetBool("isMoving", value);
+        }
+    }
+
+    [SerializeField]
+    private bool _isRunning = false;
+    
+    public bool IsRunning
+    {
+        get
+        {
+            return _isRunning;
+        } set
+        {
+            _isRunning = value;
+            animator.SetBool("isRunning", value);
+        }
+    }
+    public bool _isFacingRight = true;
+    public bool IsFacingRight
+    {
+        get
+        {
+            return _isFacingRight;
+        }
+        private set
+        {
+            if (_isFacingRight != value)
+            {
+                //flip the local scale to make the player face the opposite direction
+                transform.localScale *= new Vector2(-1, 1);
+            }
+            _isFacingRight = value;
+        }
+    }
+
+    Rigidbody2D rb;
+    Animator animator;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(moveInput.x*CurrentMoveSpeed, rb.linearVelocity.y);
+    }
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+
+        IsMoving = moveInput != Vector2.zero;
+
+        SetFacingDirection(moveInput);
+    }
+    private void SetFacingDirection(Vector2 moveInput)
+    {
+        if(moveInput.x > 0 && !IsFacingRight)
+        {
+            //face the right
+            IsFacingRight = true;
+        } else if (moveInput.x < 0 && IsFacingRight)
+        {
+            //face the left
+            IsFacingRight = false;
+        }
+    }
+    public void OnRun(InputAction.CallbackContext context)
+    {
+        if (context.started) {
+            IsRunning = true;
+
+        } else if (context.canceled) {
+            IsRunning = false;
+        }
+    }
+}
